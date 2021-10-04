c7d6a8998f6d7c099b1902a32e13adfb9e953e12
jagadish <jagadishreddy411@gmail.com>
2021-10-04 16:11:36 +0530

    HRM System

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>@<a href="#">Jagadish </a></b>
+    </div>
+    <strong>Copyright &copy; 2021 Human Resource Management System</strong>
+</footer>
